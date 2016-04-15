e6bc11342eb69bf9342cb725697c6b830e915f7a
Max Mathys <threadox@gmail.com>
2016-04-15 17:11:46 +0200

    some changes? oh yes gp

--- convertGP.sh
+++ convertGP.sh
@@ -2,5 +2,6 @@ for file in ./gp5/*
 do
   filename=$(basename "$file")
   filename="${filename%.*}"
-  java -jar converter/TabConverter.jar "$file" midi "./midi/$filename.midi"
+  timeout 10 java -jar converter/TabConverter.jar "$file" midi "./midi/$filename.midi" > /dev/null 2>&1
+  echo "$filename"
 done
